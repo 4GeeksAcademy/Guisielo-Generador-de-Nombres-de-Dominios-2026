9b815a656eaf3de1e5a4e458f22a910a348dd866
Guisielo <155173663+Guisielo@users.noreply.github.com>
2026-04-10 22:45:41 +0000

    Cambios realizados por Guisella

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon','surfnet','colorio','colones','campus'];
+  let extensiones = ['.com', '.net', '.us', '.io', '.es'];
+
+  const contenedor = document.getElementById("dominios");
+  contenedor.innerHTML = "";
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let ext of extensiones) {
+          const extSinPunto = ext.slice(1);
+          let resultado;
+
+          if (n.endsWith(extSinPunto)) {//dominio hackeado
+            resultado =
+              `${p}${a}${n.slice(0, n.length - extSinPunto.length)}${ext}  &lt;-- hackeado 😎`;
+          } else {
+            resultado = `${p}${a}${n}${ext}`;
+          }
+          contenedor.innerHTML += `${resultado}<br>`;
+          console.log(resultado.replace("&lt;", "<"));
+        }
+      }
+    }
+  }
+  //console.log("Hello Rigo from the console!");
 };
